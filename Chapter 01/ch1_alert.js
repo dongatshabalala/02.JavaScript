46c6ce89da5998c1c7d1e29b2d3d1de866e2349c
Donga Tshabalala <dongatshabalala.dev@gmail.com>
2024-08-19 22:41:34 +0200

    Chapter 1

---
+++ Chapter 01/ch1_alert.js
@@ -0,0 +1,9 @@
+"use strict"; /* important for formatting purposes, ensures that everything
+                 is as accurate as it can be*/
+
+alert("Saying hi from a different file!");
+/*indents make code easier to read*/
+
+
+let myName = prompt("What is your name?");
+alert("Hi there, ${myName}.");
